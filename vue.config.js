054213c8cc575af8db934d0a882c930e9bffeb41
pauft <1195566313@qq.com>
2020-11-05 20:09:21 +0800

    rem

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 
 module.exports = {
     lintOnSave: true,
+    outputDir: 'dist',
     css: {
         loaderOptions: {
             postcss: {
                 plugins: [
                     require('postcss-pxtorem')({
-                        rootValue : 1, // 换算的基数
+                        rootValue : 37.5, // 换算的基数
                         //selectorBlackList  : ['weui','mu'], // 忽略转换正则匹配项
                         propList   : ['*'],
                     }),
