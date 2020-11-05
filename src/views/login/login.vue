8cb3b6c21e8b9824b71124cb6215cb139576aafa
pauft <1195566313@qq.com>
2020-11-05 20:02:25 +0800

    init

---
+++ src/views/login/login.vue
@@ -0,0 +1,18 @@
+<template>
+       <button @click="login">登录</button>
+</template>
+<script>
+export default {
+    methods:{
+        login () {
+            this.$router.replace({
+                path:"/index"
+            })
+        }
+    }
+}
+</script>
+
+<style lang="less">
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+    lintOnSave: true,
+    css: {
+        loaderOptions: {
+            postcss: {
+                plugins: [
+                    require('postcss-pxtorem')({
+                        rootValue : 1, // 换算的基数
+                        //selectorBlackList  : ['weui','mu'], // 忽略转换正则匹配项
+                        propList   : ['*'],
+                    }),
+                ]
+            }
+        }
+    },
+}
